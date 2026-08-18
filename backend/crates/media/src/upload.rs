95b2fa9f5a4c3ff8f9b39536cf7a69c0e2c41817
thecybro <121627329+thecybro@users.noreply.github.com>
2026-08-18 20:32:27 +0545

    feat: add a chunk writing logic to write bytes chunk by chunk as the stream arrives Also make so that the filename would be .part before writing is finished and .final after everything is done

--- backend/crates/media/src/upload.rs
+++ backend/crates/media/src/upload.rs
@@ -8,8 +8,13 @@ use axum::{
     }  
 };
 use futures_util::StreamExt;
+use tokio::{
+    fs::{self, File},
+    io::AsyncWriteExt
+};
 
-const MAX_UPLOAD_BYTES: u64 = 1024;
+// 3MB limit
+const MAX_UPLOAD_BYTES: u64 = 3*1024*1024; // 1MB = 1,048,576
 
 pub async fn handle_media_upload(request: Request) -> (StatusCode, String) {
     let ok = StatusCode::OK;
@@ -18,14 +23,14 @@ pub async fn handle_media_upload(request: Request) -> (StatusCode, String) {
         return err;
     }
 
-    let bytes = match handle_media_body(request).await {
-        Ok(bytes) => bytes,
+    let (bytes, filepath) = match handle_media_body(request).await {
+        Ok((bytes, filepath)) => (bytes, filepath),
         Err(err) => return err,
     };
 
     (
         ok,
-        format!("{ok}: Received {bytes} bytes!\n")
+        format!("{ok}: File with bytes {bytes} was created on {filepath}!\n")
     )
     
 }
@@ -70,21 +75,57 @@ fn handle_media_header(request: &Request) -> Result<(), (StatusCode, String)> {
     }
 }
 
-async fn handle_media_body(request: Request) -> Result<u64, (StatusCode, String)> {
+async fn handle_media_body(request: Request) -> Result<(u64, String), (StatusCode, String)> {
     let payload_too_large = StatusCode::PAYLOAD_TOO_LARGE;
     let bad_request = StatusCode::BAD_REQUEST;
+    let internal_server_error = StatusCode::INTERNAL_SERVER_ERROR;
     
     let body = request.into_body();
     let mut stream = body.into_data_stream();
 
     let mut total_bytes: u64 = 0;
 
+    // relative to where code is ran from
+    let mut path = std::path::PathBuf::from("./uploads"); 
+    match fs::create_dir_all(&path).await {
+        Ok(()) => {},
+        Err(_) => {
+            return Err((
+                internal_server_error,
+                format!("{internal_server_error}: Couldn't create the required directories!\n")
+            ))
+        }
+    }
+    path.push("filename.part");
+
+    let mut file = match File::create(&path).await {
+        Ok(f) => f,
+        _ => {
+            let filepath = path.display();
+            return Err((
+                internal_server_error,
+                format!("{internal_server_error}: Couldn't create file '{filepath}'!\n")
+            ))
+        }
+    };
+    
     while let Some(stream_chunk) = stream.next().await {
         match stream_chunk {
             Ok(chunk) => {
                 match total_bytes.checked_add(chunk.len() as u64) {
                     Some(new_total) if new_total <= MAX_UPLOAD_BYTES => {
-                        total_bytes = new_total
+                        total_bytes = new_total;
+                        
+                        match file.write_all(&chunk).await {
+                            Ok(_) => {},
+                            _ => {
+                                let filepath = path.display();
+                                return Err((
+                                    internal_server_error,
+                                    format!("{internal_server_error}: Couldn't write bytes to file '{filepath}'!\n")
+                                ))
+                            }
+                        }
                     },
                     _ => {
                         return Err((
@@ -102,6 +143,30 @@ async fn handle_media_body(request: Request) -> Result<u64, (StatusCode, String)
             }
         }
     }
-    
-    Ok(total_bytes)
+
+    match file.flush().await {
+        Ok(()) => {
+            let old_path = path.clone();
+            path.set_file_name("filename.final");
+
+            match tokio::fs::rename(&old_path, &path).await {
+                Ok(_) => Ok((
+                    total_bytes,
+                    path.display().to_string()
+                )),
+                Err(_) => {
+                    return Err((
+                        internal_server_error,
+                        format!("{internal_server_error}: Couldn't finalize the name of the file!\n")
+                    ))
+                }
+            }
+        },
+        Err(e) => {
+            return Err((
+                internal_server_error,
+                format!("{internal_server_error}: Error '{e}' while flushing bytes into file!\n")
+            ))
+        }
+    }
 }
